a714d19f9353381e352faa53704e27895c112f80
ArtemiyKalinin <9776020@mail.ru>
2023-09-06 01:14:58 +0300

    Seminar3

---
+++ sem_4/Classwork/src/Task5/Main.java
@@ -0,0 +1,18 @@
+package Task5;
+
+class A {
+    // telo classa
+}
+
+class B extends A {
+     // telo classa   
+}
+
+class C extends A{
+    // telo classa
+}
+
+class D {
+    // telo classa    
+}
+
